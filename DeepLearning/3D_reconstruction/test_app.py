135c92bac68c8f159b8ba8e9f0952d4c5cc3a4ae
Kyle Paul <kylepaul1501@gmail.com>
2023-10-17 15:24:53 +0700

    create app

--- DeepLearning/3D_reconstruction/test_app.py
+++ DeepLearning/3D_reconstruction/test_app.py
@@ -1,3 +1,4 @@
+import customtkinter
 import nibabel as nib
 import numpy as np
 from stl import mesh
@@ -7,6 +8,15 @@ from vedo.applications import RayCastPlotter
 import os
 import SimpleITK as sitk
 from tqdm import tqdm
+
+customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+
+app = customtkinter.CTk()
+app.title('VasculAR software')
+app.geometry("1600x800")
+app.iconbitmap('imgs/logo.ico')
 
 # Define a function to load and smooth a mesh from a file
 def load_mesh(filename, color, texture):
@@ -23,8 +33,9 @@ def show_mesh(path):
     files = [f for f in os.listdir(path) if f.endswith('.stl')]
     buttons = []
     plt = Plotter()
+    progress = tqdm(total=len(files))
 
-    for i in tqdm(range(len(files))):
+    for i in range(len(files)):
         if files[i].endswith('.stl'):
             print(path + files[i])
             mesh = load_mesh(path + files[i], colors[i], textures[i])
@@ -52,8 +63,47 @@ def show_mesh(path):
             )
             
             buttons.append(button)
-        
+            
+        # Update the tqdm object with one iteration and set the postfix to the current file name
+        progress.update(1)
+        progress.set_postfix(file=files[i])
+
+        # Update the reconstruction_progress_bar with the current value of tqdm
+        reconstruction_progress_bar.set(int(round(progress.n / progress.total * 100, 0)))
+        print('PROGRESS BAR:', int(round(progress.n / progress.total * 100, 0)))
+            
     plt = show(meshes, bg='black')
     plt.show()
+
+def start_reconstruction():
+    reconstruction_progress_bar.start()
+    show_mesh('../data/MM_WHS/seg_res/1006/')
     
-show_mesh('../data/MM_WHS/seg_res/1006/')
+def cancel_reconstruction():
+    reconstruction_progress_bar.stop()
+
+reconstruction_progress_bar = customtkinter.CTkProgressBar(app, orientation="horizontal")
+reconstruction_progress_bar.set(0.1)
+reconstruction_progress_bar.pack(pady=20)
+
+reconstruction_btn = customtkinter.CTkButton(app, text="Automatic 3D reconstruction", command=start_reconstruction)
+reconstruction_btn.pack(padx=10, pady=21)
+
+cancel_reconstruction_btn = customtkinter.CTkButton(app, text="Cancel process", command=cancel_reconstruction)
+cancel_reconstruction_btn.pack(padx=10, pady=21)
+
+def change_colormap(choice):
+    output_label.configure(text=choice)
+
+text = customtkinter.CTkLabel(app, text="Pick a color")
+text.pack(pady=40)
+
+colors = ["Spectrum", "Fire", "Hot-and-cold", "Gold", "Overlay", "Red Overlay", "Green overlay", "Blue overlay"]
+default_combox = customtkinter.StringVar(value="Select colormap")
+colors_selection = customtkinter.CTkComboBox(app, values=colors, command=change_colormap, variable=default_combox)
+colors_selection.pack(pady=42)
+
+output_label = customtkinter.CTkLabel(app, text="")
+output_label.pack(pady=46)
+
+app.mainloop()
